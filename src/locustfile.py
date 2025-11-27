4a386cbce990fdf54cf51f7285b9c272781d202c
Alexfx33 <lovemilalex@gmail.com>
2025-11-27 02:39:51 -0300

    config: teste 3

--- src/locustfile.py
+++ src/locustfile.py
@@ -23,8 +23,8 @@ CONF = {
     'client.id': 'locust-benchmark',
     
     # OTIMIZAÇÃO DE THROUGHPUT (Vazão)
-    'linger.ms': 10,           # Aguarda até 10ms para encher um lote (batch)
-    'batch.size': 131072,       # Tamanho máximo do lote: 64KB
+    'linger.ms':  0,           # Aguarda até 10ms para encher um lote (batch)
+    'batch.size': 65536,       # Tamanho máximo do lote: 64KB
     'compression.type': 'lz4', # Compactação rápida (economiza rede)
     'acks': '1',               # 1 = Líder confirma (Rápido). 'all' = Lento/Seguro.
     'queue.buffering.max.messages': 100000, # Fila local grande para não travar
